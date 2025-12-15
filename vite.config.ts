0c080f04c33d01b4e020d95c1779b9194f01467c
akimhac <akim.hachili@gmail.com>
2025-12-15 22:02:21 +0000

    FIX: Calibration Vite Config for Root Domain (kilolab.fr)

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  base: '/', // Force la racine du site
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   },
+  server: {
+    host: true
+  }
 });
